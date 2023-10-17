c62cc64f7251a7264fc7b42db9dea438c5459b1d
Vasya Pupkin <vasya@pupkin.ru>
2023-10-17 23:21:00 +0300

    ''

--- ИДЗ_1/array_work.asm
+++ ИДЗ_1/array_work.asm
@@ -4,16 +4,16 @@
 		lw t2(s0)
 		beqz t2, change #go to change t2 equal 0 on t6 which equal min
 		sw t2(s2) #enter t2 in B array
-		addi s2 s2 4
-		addi s0 s0 4 
+		addi s2 s2 4 # Array A
+		addi s0 s0 4 # Array B
 		addi t3 t3 1 # Update counters 
 		blt t3 s1 change_zero_on_min #check counter t3
 		li t3, 0 #reset
 		ret
 	change:  
 		sw t6(s2) # Enter min value in B array in exchange for zero
-		addi s0 s0 4
-		addi s2 s2 4
+		addi s0 s0 4 # array A
+		addi s2 s2 4 # array B
 		addi t3 t3 1 # Update counters
 		j change_zero_on_min
 		
